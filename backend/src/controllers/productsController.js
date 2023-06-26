89cc6f9e707a13d434a3c8f8a9c618a4d57f2f4b
Kshamenk Benitez <kbelpirata007@gmail.com>
2023-06-26 18:56:26 -0300

    185826062023

--- backend/src/controllers/productsController.js
+++ backend/src/controllers/productsController.js
@@ -1,29 +1,74 @@
-const axios = require("axios")
+const axios = require("axios");
+const { Product } = require("../db/db.js");
 
-const getProductsController = async() => {
-    const products = await axios.get('https://dummyjson.com/products')
-    console.log(products)
-    return products
-
-}
-const createProductController = async (title, description, price, discountPercentage, rating, stock, brand, category, thumbnail, images) => {
-    return await Product.create({title, description, price, discountPercentage, rating, stock, brand, category, thumbnail, images})
-}
-const updateProductController = async (id, {title, description, price, discountPercentage, rating, stock, brand, category, thumbnail, images}) => {
-    const product = await Product.findByPk(id)
-    await product.update({title, description, price, discountPercentage, rating, stock, brand, category, thumbnail, images})
-    return product
-}
+const getProductsController = async () => {
+  const products = await axios.get("https://dummyjson.com/products");
+  return products;
+};
+const createProductController = async (
+  title,
+  description,
+  price,
+  discountPercentage,
+  rating,
+  stock,
+  brand,
+  category,
+  thumbnail,
+  images
+) => {
+  return await Product.create({
+    title,
+    description,
+    price,
+    discountPercentage,
+    rating,
+    stock,
+    brand,
+    category,
+    thumbnail,
+    images,
+  });
+};
+const updateProductController = async (
+  id,
+  {
+    title,
+    description,
+    price,
+    discountPercentage,
+    rating,
+    stock,
+    brand,
+    category,
+    thumbnail,
+    images,
+  }
+) => {
+  const product = await Product.findByPk(id);
+  await product.update({
+    title,
+    description,
+    price,
+    discountPercentage,
+    rating,
+    stock,
+    brand,
+    category,
+    thumbnail,
+    images,
+  });
+  return product;
+};
 const deleteProductController = async (id) => {
-const productDelete = await Product.findByPk(id)
-await productDelete.destroy()
-return productDelete
-}
-
+  const productDelete = await Product.findByPk(id);
+  await productDelete.destroy();
+  return productDelete;
+};
 
 module.exports = {
-    getProductsController,
-    createProductController,
-    updateProductController,
-    deleteProductController
-}
+  getProductsController,
+  createProductController,
+  updateProductController,
+  deleteProductController,
+};
--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -1,53 +1,55 @@
-const {DataTypes} = require("sequelize")
+const { DataTypes } = require("sequelize");
 
 module.exports = (database) => {
-    database.define("Product",{
-        id : {
-            type : DataTypes.UUID,
-            primaryKey : true,
-            defaultValue: DataTypes.UUIDV4
-        },
-        title : {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                notEmpty: {
-                  msg: "Name is required"
-                }
-              }
-        },
-        description: {
+  database.define(
+    "Product",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      title: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Name is required",
+          },
         },
-        price: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        discountPercentage: {
-            type: DataTypes.DECIMAL,
-            allowNull: false
-        },
-        rating : {
-            type: DataTypes.DECIMAL,
-            allowNull: false
-        },
-        stock: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        brand: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        category: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        thumbnail: {
-            type:DataTypes.STRING
-        }
-    
-
-    },{timestamps: false})
-}
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      discountPercentage: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+      },
+      rating: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      brand: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      category: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      thumbnail: {
+        type: DataTypes.STRING,
+      },
+    },
+    { timestamps: false }
+  );
+};
