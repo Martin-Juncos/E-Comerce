8b278adb8538cfe39184b0c23d9515088d8d6527
Kshamenk Benitez <kbelpirata007@gmail.com>
2023-06-26 17:35:43 -0300

    26062023

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,10 @@
 const server = require("./app")
+const { database } = require("./src/db/db.js")
 
-server.listen(3001, ()=> console.log("Escuchando al puerto 3001"))
+database.sync({force : true })
+.then( () => {
+    server.listen(3001, ()=> console.log('Escuchando al puerto 3001'))
+})
+.catch(err => {
+    console.log(err)
+})
---
+++ backend/src/models/Product.js
@@ -0,0 +1,53 @@
+const {DataTypes} = require("sequelize")
+
+module.exports = (database) => {
+    database.define("Product",{
+        id : {
+            type : DataTypes.UUID,
+            primaryKey : true,
+            defaultValue: DataTypes.UUIDV4
+        },
+        title : {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: {
+                  msg: "Name is required"
+                }
+              }
+        },
+        description: {
+        type: DataTypes.STRING,
+        allowNull: false
+        },
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        discountPercentage: {
+            type: DataTypes.DECIMAL,
+            allowNull: false
+        },
+        rating : {
+            type: DataTypes.DECIMAL,
+            allowNull: false
+        },
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        brand: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        category: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        thumbnail: {
+            type:DataTypes.STRING
+        }
+    
+
+    },{timestamps: false})
+}
