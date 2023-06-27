4a9322540515d4601ddb473efac6e3f9a25fc5d4
Carlos Martin Juncos <mjuncos@soyhenry.com>
2023-06-27 09:56:15 -0300

    270620230955

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -8,30 +8,25 @@ function NavBar() {
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
           <a className="navbar-brand" href="/home">
-            
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-              <img className="w-50" src={smartech2} alt="img" />
-            </button>
+            <img src={smartech2} alt="smartech2" className = 'w-50' />
           </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              
               <li className="nav-item">
                 <a className="nav-link" href="/about">
                   <h5>Nosotros</h5>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Link2
                 </a>
               </li>
               <li className="nav-item dropdown">
@@ -56,11 +51,19 @@ function NavBar() {
                     </a>
                   </li>
                   <li>
+                    <hr className="dropdown-divider"></hr>
+                  </li>
+                  <li>
                     <a className="dropdown-item" href="/">
                       Something else here
                     </a>
                   </li>
                 </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled" href="/">
+                  Disabled
+                </a>
               </li>
             </ul>
             <form className="d-flex" role="search">
