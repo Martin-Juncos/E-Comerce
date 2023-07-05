6c94b75fb02ad566f5d9de16a5bc9b8a49fa3a40
Carlos Martin Juncos <mjuncos@soyhenry.com>
2023-07-05 10:55:28 -0300

    1054

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 import style from './Card.module.css'
 
 function Card(props) {
-  const { id, title, images, description, price, rating } = props;
+  const { id, title, images, price, rating } = props;
   return (
     <div key={id}  className={style.block}>
       <Link to= {`/detail/${id}`}>
@@ -11,7 +11,7 @@ function Card(props) {
         <div className= {style.neon}>
           <div className="info">
             <h5 className={style.nombre}>{title}</h5>
-            <p className="titulo">{description}</p>
+            {/* <p className="titulo">{description}</p> */}
             <h5>U$d {price}</h5>
             {
               [...new Array(5)].map((start, index) => {
