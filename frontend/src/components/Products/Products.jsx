696e1951a4335f73fae8f98cec2c74717d21a080
Kshamenk <122121634+Kshamenk@users.noreply.github.com>
2023-07-19 12:31:30 -0300

    12:31 guardando cambios

--- frontend/src/components/Products/Products.jsx
+++ frontend/src/components/Products/Products.jsx
@@ -42,3 +42,39 @@ function Products() {
 }
 
 export default Products
+
+
+// import React, { useEffect, useState } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { allProducts, deleteProduct } from '../../Redux/actions';
+
+// function Products() {
+//   const dispatch = useDispatch();
+//   const products = useSelector((state) => state.allProducts);
+
+//   useEffect(() => {
+//     dispatch(allProducts());
+//     return () => {
+//         setProductos([])
+//       // Opcional: Puedes dejar esta parte en blanco si no necesitas limpiar el estado local al desmontar el componente
+//     };
+//   }, [dispatch]);
+
+//   const handleDeleteProduct = async (id) => {
+//      dispatch( deleteProduct(id));
+//   };
+//   const [productos, setProductos] = useState([]);
+//   return (
+//     <div>
+//       {products.map((prod) => (
+//         <div key={prod.id}>
+//           <h1>{prod.title}</h1>
+//           <button>🔄</button>
+//           <button onClick={() => handleDeleteProduct(prod.id)}>❌</button>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default Products;
