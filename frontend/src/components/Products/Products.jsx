07634c57886b2bca500960cfb6f43daca04810ee
Kshamenk <122121634+Kshamenk@users.noreply.github.com>
2023-07-21 12:11:49 -0300

    Producs.module.css UpdateProduct.module.css
    se añadieron ficheros, se comenzo a trabajr en el CommentModal.jsx

--- frontend/src/components/Products/Products.jsx
+++ frontend/src/components/Products/Products.jsx
@@ -1,88 +1,45 @@
-// import React, { useEffect, useState } from 'react'
-// import { useDispatch, useSelector } from 'react-redux'
-// import { allProducts, deleteProduct } from '../../Redux/actions'
-
-
-
-// function Products() {
-//     const dispatch = useDispatch()
-//     var products = useSelector((state) => state.allProducts)
-
-//     useEffect(() => {
-//         dispatch(allProducts())
-//         return () => {
-//             setProducts([]);
-//         }
-//     }, [dispatch])
-
-//     var [products, setProducts] = useState([]);
-
-
-
-//     const handleDeleteProduct = (id) => {
-//         dispatch(deleteProduct(id))
-//     }
-
-
-//     return (
-//         <div>
-//             {
-//                 products.map((prod) => {
-//                     return (
-//                         <div>
-//                             <h1>{prod.title}</h1>
-//                             <button>🔄</button>
-//                             <button onClick={() => handleDeleteProduct(prod.id)}>❌</button>
-//                         </div>
-//                     )
-//                 })
-//             }
-//         </div>
-//     )
-// }
-
-// export default Products
-
 
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { allProducts, deleteProduct } from '../../Redux/actions';
-import UpdateProduct from '../UpdateProduct/UpdateProduct';
+
 import { Link } from 'react-router-dom';
+import styles from './Products.module.css'; // Importar los estilos
 
 function Products() {
   const dispatch = useDispatch();
   const products = useSelector((state) => state.allProducts);
 
-
   useEffect(() => {
     dispatch(allProducts());
-    return () => {
-    };
+    return () => {};
   }, [dispatch]);
 
   const handleDeleteProduct = (id) => {
     dispatch(deleteProduct(id));
   };
 
-  
-
   return (
-    <div>
+    <div className={styles.container}>
       {products.map((prod) => (
-        <div key={prod.id}>
+        <div key={prod.id} className={styles.product}>
           <h2>{prod.title}</h2>
           <h4>{prod.description}</h4>
-          <h4> ${prod.price}</h4>
+          <h4>${prod.price}</h4>
           <h4>{prod.discountPercentage}</h4>
           <h4>{prod.rating}</h4>
           <h4>{prod.stock}</h4>
           <h4>{prod.brand}</h4>
           <h4>{prod.category}</h4>
           <Link to={`../updateproduct/${prod.id}`}>
-          <button>🔄</button>
+            <button className={`${styles.updatebtn}`}>🔄</button>
           </Link>
-          <button onClick={() => handleDeleteProduct(prod.id)}>❌</button>
+          <button
+            onClick={() => handleDeleteProduct(prod.id)}
+            className={`${styles.deletebtn}`}
+          >
+            ❌
+          </button>
         </div>
       ))}
     </div>
