58625d0c4745bf89d806c5c0805cbea06006c03d
Kshamenk Benitez <122121634+Kshamenk@users.noreply.github.com>
2023-08-10 17:44:00 -0300

    17;43

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -6,9 +6,6 @@ import { useAuth0 } from "@auth0/auth0-react";
 export const Login = () => {
 
     const { loginWithRedirect } = useAuth0();
-
-
-
     return(
         <button className={styles.boton} onClick={() => loginWithRedirect()}>Ingresar! 🚪</button>
     )
