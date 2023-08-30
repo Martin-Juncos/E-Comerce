514423ca540a3bed4e7b1652d6eb2dc6ea557c1b
Kshamenk Benitez <122121634+Kshamenk@users.noreply.github.com>
2023-08-30 17:11:32 -0300

    17:11

--- frontend/src/components/Users/Users.jsx
+++ frontend/src/components/Users/Users.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import style from "./Users.module.css"
 
 function Users() {
+
+  useEffect( ()=>{}),[]
   return (
     <div className={style.container} >Users</div>
   )
